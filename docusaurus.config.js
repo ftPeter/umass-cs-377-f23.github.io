4e9c5c20775c8780a6c13c8eb0187fea3ec52c1d
Tim Richards <richards@cs.umass.edu>
2023-02-20 06:39:45 -0500

    add: lec 5 and mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,51 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'COMPSCI 377',
-  tagline: 'Operating Systems',
-  url: 'https://umass-cs-377.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "COMPSCI 377",
+  tagline: "Operating Systems",
+  url: "https://umass-cs-377.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
   trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'umass-cs-377', // Usually your GitHub org/user name.
-  projectName: 'umass-cs-377.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "umass-cs-377", // Usually your GitHub org/user name.
+  projectName: "umass-cs-377.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+
   plugins: [
-    require.resolve('docusaurus-lunr-search'),
-    './src/plugins/webpack_mod'
+    require.resolve("docusaurus-lunr-search"),
+    "./src/plugins/webpack_mod",
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -54,14 +59,14 @@ const config = {
           showReadingTime: true,
           blogTitle: "Announcements",
           blogDescription: "Announcements",
-          blogSidebarTitle: "Recent Announcements"
+          blogSidebarTitle: "Recent Announcements",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -70,8 +75,14 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
       navbar: {
-        title: 'Home',
+        title: "Home",
         // logo: {
         //   alt: 'My Site Logo',
         //   src: 'img/logo.svg',
@@ -84,8 +95,8 @@ const config = {
           //   label: 'Information',
           // },
           // { to: '/docs/intro', label: 'Boo', position: 'left' },
-          { to: '/blog', label: 'Announcements', position: 'left' },
-          { to: 'docs/intro', label: 'Material', position: 'left' },
+          { to: "/blog", label: "Announcements", position: "left" },
+          { to: "docs/intro", label: "Material", position: "left" },
           // { type: 'doc', docId: 'index', to: '/lectures', label: 'Lectures', position: 'left' },
           // {
           //   href: 'https://github.com/facebook/docusaurus',
@@ -95,52 +106,52 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Material',
+            title: "Material",
             items: [
               {
-                label: 'Syllabus',
-                to: 'docs/information/syllabus',
+                label: "Syllabus",
+                to: "docs/information/syllabus",
               },
               {
-                label: 'Schedule',
-                to: 'docs/information/schedule',
+                label: "Schedule",
+                to: "docs/information/schedule",
               },
               {
-                label: 'Lectures',
-                to: 'docs/lectures',
+                label: "Lectures",
+                to: "docs/lectures",
               },
             ],
           },
           {
-            title: 'Assignments',
+            title: "Assignments",
             items: [
               {
-                label: 'Projects',
-                to: 'docs/information/schedule/#projects',
+                label: "Projects",
+                to: "docs/information/schedule/#projects",
               },
               {
-                label: 'Quizzes',
-                to: 'docs/information/schedule/#quizzes',
+                label: "Quizzes",
+                to: "docs/information/schedule/#quizzes",
               },
               {
-                label: 'Final Project',
-                to: 'docs/information/schedule/#final_project',
+                label: "Final Project",
+                to: "docs/information/schedule/#final_project",
               },
             ],
           },
           {
-            title: 'Learning Systems',
+            title: "Learning Systems",
             items: [
               {
-                label: 'Moodle',
-                href: 'https://umass.moonami.com',
+                label: "Moodle",
+                href: "https://umass.moonami.com",
               },
               {
-                label: 'Piazza',
-                href: 'https://piazza.com',
+                label: "Piazza",
+                href: "https://piazza.com",
               },
             ],
           },
